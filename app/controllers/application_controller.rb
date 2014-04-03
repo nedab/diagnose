025b97074e5912cadeb8d95337233af3a8869210
nedab <peterpink@gmx.net>
2014-04-03 21:35:57 +0200

    cache again AND touch restart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-before_filter :set_cache_buster
-
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
