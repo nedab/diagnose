3d21c6229320c0eb3e4e0c6b61238e79df1ceddf
nedab <peterpink@gmx.net>
2014-04-14 17:43:36 +0200

    re-activated rolify in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
-  #rolify
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,  :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #ROLES = %w[admin author user]
+  ROLES = %w[admin author user]
 
- # after_create :assign_default_role
- # 
- # private
- # def assign_default_role
- #   add_role(:user)
- # end
+  after_create :assign_default_role
+  
+  private
+  def assign_default_role
+    add_role(:user)
+  end
 end
